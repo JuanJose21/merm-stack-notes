e8755fa6ae142a920a649882856a9cf581fd6330
JuanJose21 <jjsanchez689@gmail.com>
2020-07-12 21:44:31 -0500

    delete node and package - add function to get and delete notes

--- frontend/src/components/CreateNote.js
+++ frontend/src/components/CreateNote.js
@@ -20,11 +20,24 @@ export default class CreateNote extends Component {
 
     getUsers = async () => {
         const res = await axios.get('http://localhost:4000/api/users');
-        this.setState({ users: res.data.map(user => user.username) });
+        this.setState({
+            users: res.data.map(user => user.username),
+            userSelected: res.data[0].username
+        });
     }
 
-    onSubmit = (e) => {
+    onSubmit = async (e) => {
         e.preventDefault();
+        const newNote = {
+            title: this.state.title,
+            content: this.state.content,
+            date: this.state.date,
+            author: this.state.userSelected
+        };
+
+        await axios.post('http://localhost:4000/api/notes', newNote);
+
+        window.location.href = '/';
     }
 
     onInputChange = e => {
@@ -34,7 +47,7 @@ export default class CreateNote extends Component {
     }
 
     onChangeDate = date => {
-        this.state({ date });
+        this.setState({ date });
     }
 
     render() {
